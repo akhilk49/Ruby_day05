e8514f79c3176a2b243444a49c4b35199500b684
akhilk49 <akhilkkurian49@gmail.com>
2025-05-28 15:48:55 +0530

    updated code using rescue blocks and debugging with pry

--- file_demo.rb
+++ file_demo.rb
@@ -1,8 +1,19 @@
 require 'colorize'
+require 'pry'
 
-File.write("content.txt", "Hey, Welcome to my Ruby Program")
+begin
+  File.write("content.txt", "Hey, Welcome to my Ruby Program")
+  content = File.read("content.txt")
 
-content = File.read("content.txt")
+  puts "The following is the content in the file content.txt: ".colorize(:blue)
+  puts content.colorize(:yellow)
 
-puts "The following is the content in the file content.txt: ".colorize(:blue)
-puts content.colorize(:yellow)
+rescue Errno::EACCES
+  puts "Permission denied while accessing the file.".colorize(:red)
+rescue Errno::ENOENT
+  puts "File not found.".colorize(:red)
+rescue => e
+  puts "An error occurred: #{e.message}".colorize(:red)
+end
+
+# binding.pry
